b241e3b211bcc4c1005c6b17d0fa0e4e69c45508
조인태 <63296903+JJongTaeng@users.noreply.github.com>
2024-03-31 14:07:32 +0900

    change project setting

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import {AppDataSource} from './data-source'
-import {User} from './entity/User'
+import { AppDataSource } from './data-source';
+import { User } from './entity/User';
 
 AppDataSource.initialize()
   .then(async () => {
-      const userRepository = AppDataSource.getRepository(User)
-      const user = new User()
-      user.age = 20
-      user.name = 'intae'
+    const userRepository = AppDataSource.getRepository(User);
+    const user = new User();
+    user.age = 20;
+    user.name = 'intae';
 
-      await userRepository.save(user) // 저장
-      await userRepository.remove(user) // 삭제
+    await userRepository.save(user); // 저장
+    await userRepository.remove(user); // 삭제
 
-      user.age = 25 // 수정 및 저장
-      await userRepository.save(user)
+    user.age = 25; // 수정 및 저장
+    await userRepository.save(user);
 
-      // 조회
-      const findUser = await userRepository.findOneBy({
-          name: 'intae'
-      })
-      console.log(findUser)
+    // 조회
+    const findUser = await userRepository.findOneBy({
+      name: 'intae',
+    });
+    console.log(findUser);
   })
-  .catch((error) => console.log(error))
+  .catch((error) => console.log(error));
